f700a1fa3b7b547048aa984512fb4aebe5f9f0c6
huajianduzhuo <yujing_m4370@163.com>
2019-04-19 10:43:48 +0800

    feat: 添加json文件存储目录

--- index.js
+++ index.js
@@ -8,7 +8,18 @@ const data = qs.parse(formData)
 
 const jsonData = JSON.stringify(data, null, 2)
 
-fs.writeFile(filename, jsonData, err => {
-  if (err) throw err
-  console.log('文件已保存')
-})
+const jsonsDirExist = fs.existsSync('./jsons')
+if (!jsonsDirExist) {
+  fs.mkdir('./jsons', err => {
+    if (err) throw err
+    write()
+  })
+} else {
+  write()
+}
+function write () {
+  fs.writeFile(`./jsons/${filename}`, jsonData, err => {
+    if (err) throw err
+    console.log('文件已保存')
+  })
+}
